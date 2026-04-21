78e2807a8f577c9a9375ee6d8679e2f45bb71719
oscar-sepulveda <ojst1236@gmail.com>
2026-04-21 08:51:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
   if (req.url === '/') {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.write('Servidor en Ubuntu funcionando 🚀');
   } else if (req.url === '/api') {
-    res.write(JSON.stringify({ status: "OK", mensaje: "API funcionando" }));
+    res.write(JSON.stringify({ mensaje: "API funcionando 🚀" }));
   } else {
     res.write('Ruta no encontrada');
   }
+
   res.end();
 });
 
